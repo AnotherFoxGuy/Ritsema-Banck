39a6a81a03a37954421e99e6777e5cc4388d7887
Edgar <Edgar@AnotherFoxGuy.com>
2020-02-20 14:33:32 +0100

    :construction_worker: Fixed travis

---
+++ backend/tests/unit/PlaceholderTest.php
@@ -0,0 +1,23 @@
+<?php 
+class PlaceholderTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+        $hello = "Hello";
+        $this->assertEquals("Hello", $hello);
+    }
+}
