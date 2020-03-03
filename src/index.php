3e31166859a6d13aebb403349d3e5b752b57351c
Edgar <Edgar@AnotherFoxGuy.com>
2020-03-03 14:55:50 +0100

    :wrench: Update project layout

--- src/index.php
+++ src/index.php
@@ -1,5 +1,6 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/lib/APIHelper.php';
+require __DIR__ . '/lib/calc.php';
 
 use RitsemaBanck\APIHelper;
 use RitsemaBanck\calc;
