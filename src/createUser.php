49e6169208d266cbbc2711bc57b0c0cd930dcabd
Edgar <Edgar@AnotherFoxGuy.com>
2020-03-16 15:40:18 +0100

    :construction: LDAP Fix

---
+++ src/createUser.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/lib/ldap_config.php';
+
+use LdapRecord\Models\OpenLDAP\User;
+use LdapRecord\Models\OpenLDAP\Group;
+
+$user = new User();
+
+$user->cn = 'test';
+$user->sn = 'John Doe';
+$user->uid = 'test'; //de gebruikersnaam
+$user->userPassword = 'test';
+
+// Save the user inside our 'Users' OU:
+$user->inside('ou=users,dc=ritsema-banck,dc=frl')->save();
+
+
+$group = Group::findOrFail('cn=web,dc=ritsema-banck,dc=frl');
