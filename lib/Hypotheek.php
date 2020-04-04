7f57c1ba642690873cc04eefe432fb5c7681d8cb
Edgar <Edgar@AnotherFoxGuy.com>
2020-04-04 11:45:08 +0200

    :tada: Port Julian

---
+++ lib/Hypotheek.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace RitsemaBanck;
+
+
+
+class Hypotheek
+{
+
+    public $application_date = "02-02-2020";
+    public $message = "testing";
+    public $edit;
+
+    //validations for Hypotheek
+
+    public function getApplication_date(){
+        return $this->application_date;
+    }
+
+    public function getMessage(){
+        return $this->message;
+    }
+
+    public function getEdit(){
+        return $this->edit;
+    }
+
+}
---
+++ src/customer/comment.php
@@ -0,0 +1,21 @@
+<?php require __DIR__ . '/../includes/navbar.php'; ?>
+
+<div class="row toppad">
+    <div class="five wide centered container">
+        <div class="ten wide container">
+            <h3>Plaats uw opmerking</h3>
+            <textarea id="comment" rows="5" cols="50"></textarea>
+        </div>
+    </div>
+</div>
+
+<div class="row toppad container">
+    <div class="five wide centered column container"></div>
+
+    <div class="two wide container">
+        <button class="twelve wide blue button"><a href="../">Verstuur</a></button>
+    </div>
+</div>
+
+
+<?php require __DIR__ . '/../includes/footer.php'; ?>
